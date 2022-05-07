05bc494f21affa00d6deff4add4b1e9bca9abbaf
Shu Tanabe <sfcofc@gmail.com>
2022-05-07 13:37:34 +0900

    [feat]一覧表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,85 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Restaurant } from "./models/restaurantModels";
 import "./App.css";
 
+const sampleRestaurants: Restaurant[] = [
+  {
+    restaurantId: "001",
+    restaurantName: "Matsuya",
+    score: 4.5,
+    introducer: "DaisukeKataoka",
+    description: "みんなの食卓でありたい",
+    updatedDate: new Date("2020-12-12"),
+  },
+  {
+    restaurantId: "002",
+    restaurantName: "Sukiya",
+    score: 4,
+    introducer: "ShuTanabe",
+    description: "すき家の牛丼",
+    updatedDate: new Date("2020-1-30"),
+  },
+];
+
+// type DisplayedRestaurant = Omit<Restaurant, "restaurantId">;
+
+// const displayedRestaurants: DisplayedRestaurant[] = [
+//   {
+//     restaurantName: "Matsuya",
+//     score: 4.5,
+//     introducer: "DaisukeKataoka",
+//     description: "みんなの食卓でありたい",
+//     updatedDate: new Date("2020-12-12"),
+// },{
+//     restaurantName: "Sukiya",
+//     score: 4,
+//     introducer: "ShuTanabe",
+//     description: "すき家の牛丼",
+//     updatedDate: new Date("2020-1-30"),
+// }
+// ]
+
+type TableHeader = {
+  restaurantName: string;
+  score: string;
+  introducer: string;
+  updatedDate: string;
+};
+
+const tableHeader: TableHeader = {
+  restaurantName: "店名",
+  score: "スコア",
+  introducer: "紹介",
+  updatedDate: "更新日",
+};
+
 function App() {
-  return <div>test</div>;
+  const [restaurantStates, setRestaurant] = useState<Restaurant[]>(
+    sampleRestaurants
+  );
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {Object.values(tableHeader).map((tableElement) => (
+              <th key={tableElement}>{tableElement}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sampleRestaurants.map((restaurant) => (
+            <tr key={restaurant.restaurantId}>
+              <td>{restaurant.restaurantName}</td>
+              <td>{restaurant.score}</td>
+              <td>{restaurant.introducer}</td>
+              <td>{restaurant.updatedDate.toISOString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/models/restaurantModels.ts
@@ -0,0 +1,8 @@
+export type Restaurant = {
+  restaurantId: string;
+  restaurantName: string;
+  score: number;
+  introducer: string;
+  description: string;
+  updatedDate: Date;
+};
